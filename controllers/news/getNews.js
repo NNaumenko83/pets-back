a177ac306959a97bc9307430d090266a2ce58f5e
Mykola <nnv2006@ukr.net>
2024-02-29 14:51:34 +0200

    UPD: getNews

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -4,7 +4,7 @@ const { ctrlWrapper } = require('../../helpers');
 const getNews = async (req, res) => {
     const { page = 1, limit = 10 } = req.query;
     const { news, count } = await newsService.getNews(page, limit);
-    console.log('news:', news);
+
     res.json({
         news,
         totalPages: Math.ceil(count / limit),
